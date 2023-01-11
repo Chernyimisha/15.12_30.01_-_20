7516a05285f4a50a38a0597b561b32c8c5b71a62
Proshkin Mikhail <chernyimisha@rambler.ru>
2023-01-12 01:49:32 +0300

    Отработаны задачи третьего семинара

---
+++ Task20/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Задача 21
+Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+A (3,6); B (2,1), -> 15.84
+A (7,-5); B (1,-1) -> 11.53*/
+
+int[] pointA = new int[2];
+int[] pointB = new int[2];
+
+Console.WriteLine("Введите координаты точки А: ");
+Console.Write("X: ");
+pointA[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+pointA[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки B: ");
+Console.Write("X: ");
+pointB[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y: ");
+pointB[1] = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"({pointA[0]},{pointA[1]})");
+Console.Write($"({pointB[0]},{pointB[1]})");
+
+double distance = Distance(pointA[0], pointA[1],
+                            pointB[0], pointB[1]);
+
+Console.Write($" ->{distance}");
+
+double Distance(int x1, int y1,
+                 int x2, int y2)
+{
+    int x = (x2 - x1) * (x2 - x1);
+    int y = (y2 - y1) * (y2 - y1);
+    double result = Math.Sqrt(x + y);
+    result = Math.Round(result, 2);
+    return result;
+}
---
+++ Task22/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+
+if (number > 0) PrintSquares(number);
+else Console.WriteLine("Введены некорректные данные");
+
+void PrintSquares(int n)
+{
+    for (int count = 1; count <= n; count++)
+    {
+        Console.WriteLine($"{count,4} {count * count,4}");
+    }
+}
+
+
+
+
